9d310de9182340091c14133ad5b568270775952e
maddojka <yuriy.soroko@gmail.com>
2024-08-01 00:09:27 +0300

    first commit

---
+++ src/main/java/com/soroko/footballleaguenotifier/FootballLeagueNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.soroko.footballleaguenotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLeagueNotifierApplication.class, args);
+    }
+
+}
