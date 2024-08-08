5c88529180567b89e67ce8fa82c6dc766166c52a
maddojka <yuriy.soroko@gmail.com>
2024-08-08 15:07:57 +0300

    mail sender + kafka + rabbit

--- src/main/java/com/soroko/footballleaguenotifier/service/RabbitConsumer.java
+++ src/main/java/com/soroko/footballleaguenotifier/service/RabbitConsumer.java
@@ -4,6 +4,7 @@ import com.soroko.footballleaguenotifier.entity.Holder;
 import com.soroko.footballleaguenotifier.repository.HolderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
+@EnableRabbit
 @RequiredArgsConstructor
 public class RabbitConsumer {
 
